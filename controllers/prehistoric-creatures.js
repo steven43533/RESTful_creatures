2b9da078e951bd5726bd1a08116ef6e4aa08690e
steven43533 <steven43533@gmail.com>
2021-11-05 17:55:00 -0400

    Added PUT&DELETE functionality to prehis_creatures

--- controllers/prehistoric-creatures.js
+++ controllers/prehistoric-creatures.js
@@ -14,6 +14,29 @@ router.get('/', (req,res) => {
 router.get('/new', (req, res) => {
     res.render('prehistoric_creatures/new.ejs')
 })
+
+// GET UPDATE FORM
+router.get('/edit/:idx', (req,res) => {
+    let RESTful_creatures = fs.readFileSync('./RESTful_creatures.json')
+    let creatures = JSON.parse(RESTful_creatures)
+
+    res.render('prehistoric_creatures/edit.ejs', {creatureId: req.params.idx, creature: creatures[req.params.idx]})
+})
+
+// UPDATE A CREATURE
+router.put('/:idx', (req,res) => {
+    let RESTful_creatures = fs.readFileSync('./RESTful_creatures.json')
+    let creatures = JSON.parse(RESTful_creatures)
+    
+    // re-assigning the name and type fields of the dino to be edited
+    creatures[req.params.idx].img_url = req.body.img_url
+    creatures[req.params.idx].type = req.body.type
+
+    // save the edited dinosaurs to the json file
+    fs.writeFileSync('./RESTful_creatures.json', JSON.stringify(creatures))
+
+    res.redirect('/prehistoric-creatures')
+})
 // SHOW ROUTE
 router.get('/:idx', (req, res) => {
     // get dinosaurs array
@@ -25,6 +48,7 @@ router.get('/:idx', (req, res) => {
     
     res.render('prehistoric_creatures/show.ejs', {myCreatures: creatures[creaturesIndex]})
 })
+
 
 // POST A NEW DINO
 
